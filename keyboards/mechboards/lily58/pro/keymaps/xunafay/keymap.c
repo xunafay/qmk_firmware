00adb04f8e06484255d45290515743eead839485
Hannah Witvrouwen <hannah@hexodine.com>
2025-03-31 10:00:15 +0200

    enable double-tap reset to bootloader

--- keyboards/mechboards/lily58/pro/keymaps/xunafay/keymap.c
+++ keyboards/mechboards/lily58/pro/keymaps/xunafay/keymap.c
@@ -3,6 +3,9 @@
 
 #include QMK_KEYBOARD_H
 
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+
 enum custom_keycodes {
     WORK_PC = SAFE_RANGE,
 };
