d1d9bd916d402786ccf1204bd5bffd6f20c1a7ac
Hannah Witvrouwen <hannah@hexodine.com>
2025-03-31 09:19:20 +0200

    add missing semicolon and delete key

--- keyboards/mechboards/lily58/pro/keymaps/xunafay/keymap.c
+++ keyboards/mechboards/lily58/pro/keymaps/xunafay/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [0] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BACKSLASH,
-  KC_TAB,   KC_Q,   KC_D,    KC_R,    KC_W,    KC_B,                     KC_J,    KC_F,    KC_U,    KC_P,    KC_P,    KC_RSFT,
+  KC_TAB,   KC_Q,   KC_D,    KC_R,    KC_W,    KC_B,                     KC_J,    KC_F,    KC_U,    KC_P,    KC_P,    KC_SEMICOLON,
   KC_LCTL,  KC_A,   KC_S,    KC_H,    KC_T,    KC_G,                     KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_M,    KC_C,    KC_V, KC_HOME,  KC_END,   KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
                            KC_LALT, KC_LGUI, KC_BSPC, KC_SPC, KC_ENT, MO(2), KC_LBRC, KC_RBRC
@@ -37,17 +37,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [1] = LAYOUT(
     KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BACKSLASH,
-    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RSFT,
-    KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,    KC_QUOT,
+    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SEMICOLON,
+    KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_QUOT,
     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_HOME,  KC_END,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-                             KC_LALT, KC_LGUI, KC_BSPC, KC_SPC, KC_ENT, MO(2), KC_LBRC, KC_RBRC
+                             KC_LALT, KC_LGUI, KC_DELETE, KC_SPC, KC_ENT, MO(2), KC_LBRC, KC_RBRC
     ),
 
 [2] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,    KC_F10,       KC_F11,  KC_F12,
-  _______, _______, _______, _______, _______, _______,                   WORK_PC, KC_PAGE_UP, KC_UP, KC_PAGE_DOWN, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   WORK_PC, KC_PAGE_UP, KC_UP, KC_PAGE_DOWN, _______, KC_PIPE,
   _______, _______, _______, _______, _______, _______,                   _______, KC_LEFT, KC_DOWN,  KC_RIGHT,     _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS,  KC_LCBR,      KC_RCBR, KC_PIPE,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS,  KC_LCBR,      KC_RCBR, KC_EQUAL,
                              _______, _______, MO(3), _______, _______, _______, _______, _______
 ),
 
